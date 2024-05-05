43e4d3f178190f7b5d42bae65aa3f54aa24fc158
katyamar22 <katyamarcam@gmail.com>
2024-05-05 16:02:11 -0400

    updated code

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-//Event listener for when button is clicked
-//DOMcontentloaded makes sure the HTML is done loading befor executing
-//JavaScript since we are using DOM
+// Event listener for when button is clicked
+/* DOMcontentloaded makes sure the HTML is done loading befor executing
+JavaScript since we are using DOM  */
 document.addEventListener("DOMContentLoaded", function() {
+
+    // adding our result and button elements to a variable
     const result = document.getElementById('result');
     const buttons = document.querySelectorAll('.button');
     
@@ -9,14 +11,18 @@ document.addEventListener("DOMContentLoaded", function() {
     let prevOperand = '';
     let operator = '';
 
+    // Event listeners for all buttons
     buttons.forEach(button => {
         button.addEventListener('click', function() {
             const value = button.value;
 
+            // Clears screen when hitting the reset button
             if (value === 'C') {
                 clear();
+            // Calculates
             } else if (value === '=') {
                 calculate();
+            // Combines the values submitted
             } else if (value === '+' || value === '-' || value === '*' || value === '/') {
                 operator = value;
                 prevOperand = currentOperand;
@@ -28,6 +34,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+    // Function for clearing screen
     function clear() {
         currentOperand = '';
         prevOperand = '';
@@ -35,6 +42,13 @@ document.addEventListener("DOMContentLoaded", function() {
         result.value = '0';
     }
 
+    // Displays Error Message when calculation can not be executed
+    function displayError(message) {
+        result.value = message;
+
+    }
+
+    // Function to calculate input
     function calculate() {
         let resultValue;
         const prev = parseFloat(prevOperand);
@@ -53,15 +67,21 @@ document.addEventListener("DOMContentLoaded", function() {
                 resultValue = prev * current;
                 break;
             case '/':
+                if (current === 0) {
+                    displayError("ERROR");
+                    return;
+                }
                 resultValue = prev / current;
                 break;
             default:
-                return;
+                displayError("ERROR");
+                return ;
         }
 
         result.value = resultValue;
         currentOperand = resultValue.toString();
         prevOperand = '';
         operator = '';
+        
     }
 });
